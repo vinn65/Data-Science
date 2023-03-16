701e2c403728ec0b0a09287bfa3d40f5d07e9f73
sirvin01 <salimjr201@gmail.com>
2023-03-16 13:22:57 +0300

    init

--- Datascience/ml/knn.py
+++ Datascience/ml/knn.py
@@ -8,10 +8,43 @@
 import numpy as np
 import matplotlib.pyplot as mtp
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from matplotlib.colors import ListedColormap
 
 data_set = pd.read_csv("User_Data.csv")
 
 x = data_set.iloc[:,[2,3]].values
 y = data_set.iloc[:,4].values
+
+x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.2,train_size=0.8,random_state=0)
+
+
+#feature scaling
+st_x = StandardScaler()
+x_train = st_x.fit_transform(x_train)
+x_test = st_x.transform(x_test)
+
+# print(data_set.head())
+
+classifier = KNeighborsClassifier(n_neighbors=5, metric="minkowski",p=2)
+classifier.fit(x_train,y_train)
+
+y_pred  = classifier.predict(x_test)
+
+cm = confusion_matrix(y_test,y_pred)
+
+# print(cm)
+
+x_set, y_set = x_train, y_train
+
+#visualization of training set
+x1,x2 = nm.meshgrid(nm)
+
+
+
+
+
 
